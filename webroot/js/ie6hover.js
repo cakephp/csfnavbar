4467f4552af28b41f8f95fcfd02ed88089546fb2
Chris Burke <chris.burke@cakedc.com>
2015-03-20 08:02:46 +0000

    Convert tabs to spaces

--- webroot/js/ie6hover.js
+++ webroot/js/ie6hover.js
@@ -1,15 +1,15 @@
 var sfHover = function() {
-	var sfEls = document.getElementsByTagName('li');
-	for (var i = 0; i < sfEls.length; i++) {
-		sfEls[i].onmouseover = function() {
-			this.className += ' sfhover';
-		}
-		sfEls[i].onmouseout = function() {
-			this.className = this.className.replace(new RegExp(" sfhover\\b"), '');
-		}
-	}
+    var sfEls = document.getElementsByTagName('li');
+    for (var i = 0; i < sfEls.length; i++) {
+        sfEls[i].onmouseover = function() {
+            this.className += ' sfhover';
+        }
+        sfEls[i].onmouseout = function() {
+            this.className = this.className.replace(new RegExp(" sfhover\\b"), '');
+        }
+    }
 }
 
 if (window.attachEvent) {
-	window.attachEvent('onload', sfHover);
+    window.attachEvent('onload', sfHover);
 }
